cfda163f12d5fdc3c71999594bf899615d600d03
Martin <martin.dinahet@proton.me>
2026-01-08 15:55:16 +0100

    setup project structure and dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { IndexPage } from "./pages/index-page";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// biome-ignore lint/style/noNonNullAssertion: <>
+createRoot(document.querySelector("#root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
